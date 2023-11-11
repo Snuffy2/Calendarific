b8d7b256ad2dddddb2caa9cd7a8b9730130506b0
Snuffy2 <Snuffy2@users.noreply.github.com>
2023-11-10 23:41:28 -0500

    Change to SensorEntity

--- custom_components/calendarific/sensor.py
+++ custom_components/calendarific/sensor.py
@@ -2,9 +2,9 @@
 import logging
 from datetime import date
 
-from homeassistant.const import ATTR_ATTRIBUTION, CONF_NAME
+from homeassistant.components.sensor import SensorEntity, SensorStateClass
+from homeassistant.const import ATTR_ATTRIBUTION, CONF_NAME, UnitOfTime
 from homeassistant.helpers.discovery import async_load_platform
-from homeassistant.helpers.entity import Entity
 
 from .calendar import EntitiesCalendarData
 from .const import (
@@ -20,7 +20,6 @@ from .const import (
     CONF_ICON_SOON,
     CONF_ICON_TODAY,
     CONF_SOON,
-    CONF_UNIT_OF_MEASUREMENT,
     DOMAIN,
     SENSOR_PLATFORM,
 )
@@ -46,21 +45,26 @@ async def async_setup_entry(hass, entry, async_add_entities):
     return True
 
 
-class calendarific(Entity):
+class calendarific(SensorEntity):
     def __init__(self, config, reader):
         """Initialize the sensor."""
+
+        # self._attr_device_class = SensorDeviceClass.DURATION
+        self._attr_state_class = SensorStateClass.MEASUREMENT
+        self._attr_native_unit_of_measurement = UnitOfTime.DAYS
+        self._attr_suggested_unit_of_measurement = UnitOfTime.DAYS
         self.config = config
+        self._attr_unique_id = config.get("unique_id", None)
         self._holiday = config.get(CONF_HOLIDAY)
-        self._name = config.get(CONF_NAME)
-        if not self._name or self._name == "":
-            self._name = self._holiday
+        self._attr_name = config.get(CONF_NAME)
+        if not self._attr_name or self._attr_name == "":
+            self._attr_name = self._holiday
         self._icon_normal = config.get(CONF_ICON_NORMAL)
         self._icon_today = config.get(CONF_ICON_TODAY)
         self._icon_soon = config.get(CONF_ICON_SOON)
         self._soon = config.get(CONF_SOON)
         self._date_format = config.get(CONF_DATE_FORMAT)
-        self._unit_of_measurement = config.get(CONF_UNIT_OF_MEASUREMENT)
-        self._icon = self._icon_normal
+        self._attr_icon = self._icon_normal
         self._reader = reader
         self._description = self._reader.get_description(self._holiday)
         self._date = self._reader.get_date(self._holiday)
@@ -68,41 +72,17 @@ class calendarific(Entity):
             self._attr_date = self._date
         else:
             self._attr_date = self._date.strftime(self._date_format)
-        self._state = "unknown"
-
-    @property
-    def unique_id(self):
-        """Return a unique ID to use for this sensor."""
-        return self.config.get("unique_id", None)
-
-    @property
-    def name(self):
-        """Return the name of the sensor."""
-        return self._name
-
-    @property
-    def state(self):
-        """Return the state of the sensor."""
-        return self._state
+        self._attr_native_value = None
 
     @property
     def extra_state_attributes(self):
         """Return the state attributes."""
-        # _LOGGER.debug(f"ESA {self._name} Attr Date: {self._attr_date}")
+        # _LOGGER.debug(f"ESA {self._attr_name} Attr Date: {self._attr_date}")
         return {
             ATTR_DATE: self._attr_date,
             ATTR_DESCRIPTION: self._description,
             ATTR_ATTRIBUTION: ATTRIBUTION,
         }
-
-    @property
-    def unit_of_measurement(self):
-        """Return the unit this state is expressed in."""
-        return self._unit_of_measurement
-
-    @property
-    def icon(self):
-        return self._icon
 
     async def async_added_to_hass(self):
         """Once the entity is added we should update to get the initial data loaded. Then add it to the Calendar."""
@@ -136,22 +116,20 @@ class calendarific(Entity):
     async def async_will_remove_from_hass(self):
         """When sensor is removed from hassio and there are no other sensors in the Calendarific calendar, remove it."""
         await super().async_will_remove_from_hass()
-        _LOGGER.debug(f"Removing: {self._name}")
+        _LOGGER.debug(f"Removing: {self._attr_name}")
         del self.hass.data[DOMAIN][SENSOR_PLATFORM][self.entity_id]
         self.hass.data[DOMAIN][CALENDAR_PLATFORM].remove_entity(self.entity_id)
-        _LOGGER.debug(
-            f"Remaining Calendar Entries: {self.hass.data[DOMAIN][CALENDAR_PLATFORM]}"
-        )
+        # _LOGGER.debug(f"Remaining Calendar Entries: {self.hass.data[DOMAIN][CALENDAR_PLATFORM]}")
 
     async def async_update(self):
         await self.hass.async_add_executor_job(self._reader.update)
-        _LOGGER.debug(f"Update: {self._name}")
+        _LOGGER.debug(f"Update: {self._attr_name}")
         self._description = self._reader.get_description(self._holiday)
         self._date = self._reader.get_date(self._holiday)
         # _LOGGER.debug(f"[Update] Date: {self._date}")
         # _LOGGER.debug(f"[Update] Date Type: {type(self._date)}")
         if not self._date or self._date == "-":
-            self._state = None
+            self._attr_native_value = None
             self._attr_date = self._date
             return
         self._attr_date = self._date.strftime(self._date_format)
@@ -165,9 +143,9 @@ class calendarific(Entity):
             daysRemaining = 0
 
         if daysRemaining == 0:
-            self._icon = self._icon_today
+            self._attr_icon = self._icon_today
         elif daysRemaining <= self._soon:
-            self._icon = self._icon_soon
+            self._attr_icon = self._icon_soon
         else:
-            self._icon = self._icon_normal
-        self._state = daysRemaining
+            self._attr_icon = self._icon_normal
+        self._attr_native_value = daysRemaining
